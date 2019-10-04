37e6e7842c071f1ca40d22c48333a959e9656562
JiaYongL <33950454+JiaYongL@users.noreply.github.com>
2019-10-04 21:13:28 +0800

    Update 031702126.py

--- 031702126.py
+++ 031702126.py
@@ -121,8 +121,7 @@ def getLv1(cur_addr, cur_path):
         return
 
 input_json = open('031702126.json', 'rb')
-input_Json = input_json.read() 
-data = json.load(input_Json)
+data = json.load(input_json)
 out_list = []
 #in_info = open(r'C:/031702126/in.txt', 'r', encoding='utf-8').read()
 
